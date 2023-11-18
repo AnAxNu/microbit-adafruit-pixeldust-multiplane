4ea84c15928f356b266a8880feccf8ce21150b62
AnAx <anders@anax.nu>
2023-11-18 22:28:48 +0000

    init commit

---
+++ microbit-adafruit-pixeldust-multiplane-installer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Installer script for Adafruit PixelDust multiplane using
+# electrodragon.com "RGB LED Matrix Panel Drive Board For Raspberry Pi"
+# and rpi-rgb-led-matrix with an added mapper (Row-mapper)
+
+# todo: everything
+
